77ef28ee1223a3d75e205ddbc0a16974b0ab1df8
jona24 <jalvarez2483@gmail.com>
2014-11-18 18:48:43 -0600

    creacion de carreras vista de carrera funcional

--- application/models/Carrera_model.php
+++ application/models/Carrera_model.php
@@ -1,13 +1,36 @@
 <?php
-/**
-* 
-*/
+
 class Carrera_model extends CI_Model
 {
 	
-	function __construct(argument)
+	function __construct()
 	{
 		parent::__construct();
 	}
+	function getall(){
+		$query = $this->db->get('carrera');
+        return $query->result();
+	}
+	function insert($data)
+	{
+		$this->db->insert('carrera',$data);
+	}
+	function update($id,$data){
+        $this->db->where('id',$id);
+        $this->db->update('carrera',$data);
+    }
+    function delete($id){
+        $this->db->where('id', $id);
+        $this->db->delete('carrera');
+    }
+    function getid($id){
+    	$this->db->where('id', $id);
+        $query = $this->db->get('carrera');
+        if ($query->num_rows() > 0 ){
+            return $query->row();
+        } else {
+            return null;
+        }
+    }
 }
 ?>
