0db8a135d68a2c310f30d99bb183f79ea4206cef
Diego Arrieta <diego.arrieta.r@gmail.com>
2014-12-07 13:39:59 -0600

    se repara el insertar estudiantes

--- application/models/Estudiante_model.php
+++ application/models/Estudiante_model.php
@@ -1,29 +1,33 @@
 <?php
-
 class Estudiante_model extends CI_Model
 {
-	
-	function __construct()
-	{
-		parent::__construct();
-	}
-	function getall(){
-		$query = $this->db->get('estudiante');
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+    function getall(){
+        $query = $this->db->get('estudiante');
         return $query->result();
-	}
-	function insert($data)
-	{
-		$this->db->insert('estudiante',$data);
-	}
-    function insertcarrera($data)
-    {
-        $this->db->insert('estudiante_carrera',$data);
     }
-    function insertcualidad($data2)
+    function insert($data)
     {
-        $this->db->insert('estudiante_cualidad',$data2);
+        $this->db->insert('estudiante',$data);
+        return $this->db->insert_id();
     }
-	function update($id,$data){
+    function insertcarrera($insertCarrera)
+    {
+        $this->db->insert('estudiante_carrera',$insertCarrera);
+    }
+    function insertcualidad($dato)
+    {
+        $this->db->insert('estudiante_cualidad',$dato);
+    }
+    function insertproyecto($insert)
+    {
+        $this->db->insert('estudiante_proyecto',$insert);
+    }
+    function update($id,$data){
         $this->db->where('id',$id);
         $this->db->update('estudiante',$data);
     }
@@ -32,7 +36,7 @@ class Estudiante_model extends CI_Model
         $this->db->delete('estudiante');
     }
     function getid($id){
-    	$this->db->where('id', $id);
+        $this->db->where('id', $id);
         $query = $this->db->get('estudiante');
         if ($query->num_rows() > 0 ){
             return $query->row();
